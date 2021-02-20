e3acf56cde5ddf1c2a57ae256a05e2b6329e7baa
MedhaManasvini <79033962+MedhaManasvini@users.noreply.github.com>
2021-02-20 19:50:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function alert1(){
+    window.alert("This is a beautiful landscape. Live life beautifully")
+}
+function alert2(){
+    window.alert("This a Helping. Helping people is one of the most noble job")
+}
+function alert3(){
+    window.alert("This is hardwork. Harddwork Pays")
+}
+function alert4(){
+    window.alert("this is kindness. We should always be kind")
+}
